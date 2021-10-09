cd896764034dfd6c4e2782e2202a01c0f0baf6d4
khfifi_slim <80694671+KhfifiSlim@users.noreply.github.com>
2021-10-09 21:56:49 +0100

    Add files via upload

---
+++ buggys/src/main/java/com/slim/buggys/BuggysApplication.java
@@ -0,0 +1,13 @@
+package com.slim.buggys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuggysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuggysApplication.class, args);
+	}
+
+}
